2acd659219efc815d9f0a911a63f3e0d7d400d51
malek-bachta <75339154+malek-bachta@users.noreply.github.com>
2023-09-18 10:29:55 +0100

    searcha bar style

--- Components/SearchInput.js
+++ Components/SearchInput.js
@@ -9,15 +9,13 @@ function SearchInput({ onSearch }) {
     onSearch(searchText);
   }
 
-  function search(text)
-  {
+  function search(text) {
     onSearch(text);
     setSearchText(text);
   }
-  
+
   function handleSearchButtonPress() {
     onSearch(searchText);
-
   }
 
   return (
@@ -26,10 +24,9 @@ function SearchInput({ onSearch }) {
         placeholder="Search"
         style={styles.input}
         value={searchText}
-        onChangeText={(text)=>search(text)}
-        
-        onSubmitEditing={handleSearchButtonPress} 
-        returnKeyType="search" 
+        onChangeText={(text) => search(text)}
+        onSubmitEditing={handleSearchButtonPress}
+        returnKeyType="search"
       />
       <TouchableOpacity style={styles.addButton} onPress={addToDoHandler}>
         <Iconn name="search" size={20} color="#EE9B37" />
@@ -38,28 +35,27 @@ function SearchInput({ onSearch }) {
   );
 }
 
-
 const styles = StyleSheet.create({
   inputContainer: {
-      flexDirection: "row",
-    backgroundColor: "#000",
-    borderRadius: 20,
-    margin:10,
+    flexDirection: "row",
+    backgroundColor: "#494037", 
+    borderRadius: 8, 
+    margin: 15,
+    alignItems: "center", 
   },
   input: {
-    flex: 4,
-    height: 50,
-    borderRadius: 25,
-    paddingHorizontal: 20,
-    paddingRight: 5,
-    color: "#fff",
+    flex: 1, 
+    height: 40, 
+    paddingHorizontal: 10, 
+    fontSize: 16, 
+    color: "#FFF", 
   },
   addButton: {
-    width: 50,
-    height: 50,
+    width: 40, 
+    height: 40, 
     alignItems: "center",
     justifyContent: "center",
-    marginLeft: 10,
+    borderRadius: 8, 
   },
 });
 
